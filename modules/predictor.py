b7761d123e31df7a4590838ab8180a0bfe01c645
Pankaj Agarwal <98219223+PankajAgarwalS@users.noreply.github.com>
2025-06-30 17:27:01 +0530

    Update predictor.py

--- modules/predictor.py
+++ modules/predictor.py
@@ -4,17 +4,17 @@ import pandas as pd
 import numpy as np
 import joblib
 from tensorflow.keras.models import load_model
-import config
+import app_config
 import os
 
 def make_predictions_for_stock(stock_code):
     """Makes future predictions for a given stock."""
-    processed_data = pd.read_csv(os.path.join(config.PROCESSED_DATA_DIR, f"{stock_code}_processed_data.csv"))
+    processed_data = pd.read_csv(os.path.join(app_config.PROCESSED_DATA_DIR, f"{stock_code}_processed_data.csv"))
     latest_data = processed_data.drop(columns=['date', 'stock_code']).iloc[-1]
 
-    rf_model = joblib.load(os.path.join(config.MODEL_DIR, f"{stock_code}_rf_model.pkl"))
-    xgb_model = joblib.load(os.path.join(config.MODEL_DIR, f"{stock_code}_xgb_model.pkl"))
-    lstm_model = load_model(os.path.join(config.MODEL_DIR, f"{stock_code}_lstm_model.h5"))
+    rf_model = joblib.load(os.path.join(app_config.MODEL_DIR, f"{stock_code}_rf_model.pkl"))
+    xgb_model = joblib.load(os.path.join(app_config.MODEL_DIR, f"{stock_code}_xgb_model.pkl"))
+    lstm_model = load_model(os.path.join(app_config.MODEL_DIR, f"{stock_code}_lstm_model.h5"))
 
     latest_data_reshaped = latest_data.values.reshape(1, -1)
     latest_data_lstm = latest_data.values.reshape(1, 1, -1)
@@ -40,7 +40,7 @@ def make_predictions_for_stock(stock_code):
 def generate_all_predictions():
     """Generates predictions for all stocks."""
     predictions = []
-    stocks = [f.split('_')[0] for f in os.listdir(config.MODEL_DIR) if f.endswith('.pkl')]
+    stocks = [f.split('_')[0] for f in os.listdir(app_config.MODEL_DIR) if f.endswith('.pkl')]
     for stock in stocks:
         predictions.append(make_predictions_for_stock(stock))
     return pd.DataFrame(predictions)
